72733f4d7461c8f11962292af3da389e02badece
1GDesaegher Hugo <1GDesaegher Hugo>
2016-11-09 15:25:54 +0100

    first commit

---
+++ src/goosegame/Cell.java
@@ -0,0 +1,39 @@
+package goosegame;
+
+/**
+ * Interface for the cells of the "jeu de l'oie" game. <br/> Note that there can be only 
+ * one player by cell, the starting cell (index 0) excepted.
+ */
+public interface Cell {
+	/**
+	 * @return <tt>true</tt> if and only if the player in this cell can freely
+	 *  leaves the cell, else he must wait for another player to reach this cell 
+	 *  or wait a given number of turns
+	 */
+	public boolean canBeLeft();
+
+	/** returns the index of this cell */
+	public int getIndex();
+
+	  /**
+     * returns the index of the cell really reached by a player when the player
+     *    reaches this cell.  For normal cells, the returned value equals
+     *   <code>getIndex()</code> and is thus independent from
+     *   <code>diceThrow</code>.
+     * @param diceThrow the result of the dice when the player reaches this cell
+     * @return the index of the actual cell where the player eventually
+     * arrives when the given throw of dice sends the player in this cell
+     */
+	public int handleMove(int diceThrow);
+
+	/** returns <tt>true</tt> iff a player is in this cell */
+	public boolean isBusy();
+
+	 /**  handles what happens when a player arrives in this cell 
+     * @param player the new player in the sell
+     */
+	public void welcomePlayer(Player player);
+
+	/** gets the player in this cell <tt>null</tt> if none */
+	public Player getPlayer();
+}// Cell
---
+++ test/PlayerTest.java
@@ -0,0 +1,58 @@
+package tests;
+
+import static org.junit.Assert.*;
+import goosegame.BasicCell;
+import goosegame.Cell;
+import goosegame.Player;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PlayerTest {
+
+	private Cell cell;
+	private Player player;
+	
+	@Before
+	public void setUpBeforeClass() throws Exception {
+		this.cell = new DummyCell();
+		this.player = new Player("playerTest");
+	}
+
+	@Test
+	public void testPlayer() {
+		assertNotNull("player created ?", player);
+	}
+
+	@Test
+	public void testGetSetCell() {
+		// same tests for setter and getter 
+		assertNull("no cell set", player.getCell());
+		player.setCell(cell);
+		assertNotNull("cell set", player.getCell());		
+		assertSame("same cell ?",cell, player.getCell());
+		assertNotSame("not same cell ?",new DummyCell(), player.getCell());
+	}
+
+	/** A useless (mock) class of interface cell only to achieve tests */
+	private class DummyCell implements Cell {
+		public boolean canBeLeft() {
+			return false;
+		}
+		public int getIndex() {
+			return 0;
+		}
+		public int handleMove(int diceThrow) {
+			return 0;
+		}
+		public boolean isBusy() {
+			return false;
+		}
+		public void welcomePlayer(Player player) {
+		}
+		public Player getPlayer() {
+			return null;
+		}
+	}
+}
